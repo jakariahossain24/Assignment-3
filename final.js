2fc934a63c18fd21149e26d44af12ae77875f339
jakariahossain24 <jakariahossain00011@gmail.com>
2025-08-04 18:13:22 +0600

    problem 3

--- final.js
+++ final.js
@@ -1,11 +1,11 @@
 /** Problem -01 ( Divide the Asset ) */
-var area = 2060;
+var area = 800;
 //write your code here
 console.log(area/2);
 
 
 /** Problem -02 ( Cycle or Laptop ) */
-var money = 9999;
+var money = 10000;
 //write your code here
 if(money >= 25000){
     console.log('Laptop');
@@ -16,6 +16,20 @@ else if(money >= 10000){
 else
 {
     console.log('Chocolate');
+}
+
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 11;
+//write your code here
+let day;
+for(day = 1 ; day <= lastDay ; day++){
+    if(day % 3 === 0){
+        console.log(day + ' - medicine')
+    }
+    else{
+        console.log(day + ' - rest')
+    }
 }
 
 
