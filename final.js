c09bc04f9d0ef103c59c94868e2ca371518c88b2
jakariahossain24 <jakariahossain00011@gmail.com>
2025-08-04 18:22:28 +0600

    problem5

--- final.js
+++ final.js
@@ -46,6 +46,10 @@ else{
 }
 
 
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "mewo" , roll: 96 ,department: "cse"};
+//write your code here
+console.log(student.name + student.roll + '.' + student.department + '@ph.ac.bd');
 
 
 
