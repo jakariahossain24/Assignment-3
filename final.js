bfc20d208aac8f5aabec40b80a812c4887f472ae
jakariahossain24 <jakariahossain00011@gmail.com>
2025-08-04 18:19:12 +0600

    problem4

--- final.js
+++ final.js
@@ -34,3 +34,18 @@ for(day = 1 ; day <= lastDay ; day++){
 
 
 
+
+/** Problem 04 - (Delete / Store) */
+var fileName= "pdfData.jpg";
+//write your code here
+if(fileName.startsWith('#') || fileName.endsWith('pdf') || fileName.endsWith('docx')){
+    console.log('Store');
+}
+else{
+    console.log('Delete');
+}
+
+
+
+
+
